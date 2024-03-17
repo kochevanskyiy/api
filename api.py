c1df75a19008a7e8a7ee4dcd05950dbeade2e5a0
Feras_shadid <Feras_shadid@yahoo.com>
2024-03-17 23:30:33 +0300

    Step2

--- api.py
+++ api.py
@@ -1,4 +1,12 @@
 from flask import Flask
+import os
+from urllib.parse import urlparse, parse_qs, unquote
+from dotenv import load_dotenv
+from langchain_pinecone import PineconeVectorStore
+from langchain_openai import OpenAIEmbeddings
+from pinecone import Pinecone
+from flask import Flask, request, jsonify
+
 
 app = Flask(__name__)
 
